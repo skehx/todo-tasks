f32eed9c70e016a4dcf5bbf2e9b48fc395fdaf3d
skehx <anthonygonzalezgd@gmail.com>
2022-02-23 21:22:25 -0500

    CRUD ok

--- api.js
+++ api.js
@@ -52,6 +52,29 @@ router.get('/all-tasks', function (req, res) {
     });
 });
 
+router.post('/update-task', function (req, res) {
+    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+        Name: req.body.Name,
+        Deadline: req.body.Deadline
+    }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
 module.exports = router;
 
 
